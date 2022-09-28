db89c78f8dc0083662e8a40d6ea71119742e807a
Diptanshu Giri <dipu12455@gmail.com>
2022-09-27 21:56:55 -0600

    commit 7.2 djikstra's algorithm fully functional but incomplete

--- dModule.py
+++ dModule.py
@@ -1,3 +1,6 @@
+DEBUG =0 
+if DEBUG: import pdb
+
 class DField:
     def __init__(self, house, shortest, prevHouse):
         self.house = house
@@ -5,12 +8,57 @@ class DField:
         self.prevHouse = prevHouse
 
     def printValues(self):
-        print(self.house + "\t" + str(self.shortest) + "\t" +
-                self.prevHouse)
+        print(str(self.house) + "\t" + str(self.shortest) + "\t" +
+                str(self.prevHouse))
+
+#1 iterates a list and generates a list of unique houses that were in that list
+def getHouseList(_stringList):
+    houseList = []
+    for i in _stringList:
+        line = i.split(",")
+        if line[1] not in houseList:
+            houseList.append(line[1])
+        if line[2] not in houseList:
+            houseList.append(line[2])
+    return houseList
+
+# initialize the dTable
+def dTableInit(_houseList):
+    dTable = []
+    for i in _houseList:
+        temp = DField(i,-1,"Z") 
+        # '-1' means infinity and 'Z' means null-string
+        dTable.append(temp)
+    return dTable
+
+# finds the vertex with the shortest distance in the dTable, but also not present in _visitedList
+# it will return '-1' if everything in the list has been visited
+def findVertexShortestDistance(_dTable, _visitedList):
+    minimum = -1
+    foundIndex = -1
+    for idx, i in enumerate(_dTable):
+        if i.house in _visitedList: # skip if already visited
+            continue
+        if minimum == -1:
+            minimum = i.shortest
+            foundIndex = idx
+            continue
+        if i.shortest == -1: # inf cannot be min, so goto next
+            continue
+        if i.shortest < minimum:
+            minimum = i.shortest
+            foundIndex = idx
+    return foundIndex # this is the index of vertex in dTable with shortest distance
+
+def findInTable(_dTable, _houseToFind): # finds the given housename (string) in the dTable and 
+    # returns its index
+    for index, i in enumerate(_dTable):
+        if i.house == _houseToFind:
+            return index
 
 def djikstraModule():
 
-    stringList = [
+    string1 = [
             "6,A,B",
             "1,A,D",
             "2,D,B",
@@ -18,80 +66,97 @@ def djikstraModule():
             "2,E,B",
             "5,E,C",
             "5,C,B"]
-    # to find connection for eg for A with other houses
-    # iterate the whole list and find for A,B or A,D
-    # that way you know what houses A is connected to
+    string2 = [
+            "1,A,F",
+            "5,A,D",
+            "3,D,F",
+            "4,F,C",
+            "6,F,B",
+            "9,D,B",
+            "11,C,B",
+            "8,C,G",
+            "2,G,E",
+            "7,B,E"
+            ]
+    stringList = string2
 
-    # before applying the djisktra algorithm,
-    # first step is to make sure the file is in a proper format
-    # no repitiions, like 6,A,B and 6,B,A
-    # no unconnected houses, no houses by themselves
-    # no street that is seperate from the rest, something 
-    # always needs to connect directly or indirectly to that street
-
-    # then, you can normally use the djikstra's algorithm with no
-    # worries of these anomalies
-
-    notVisitedList = []
-    for i in stringList:
-        x = i.split(",")
-        if x[1] not in notVisitedList:
-            notVisitedList.append(x[1])
-        if x[2] not in notVisitedList:
-            notVisitedList.append(x[2])
-    print("notVisitedList[]:")
-    print(notVisitedList)
-
-    # create a dTable from the nonvisitedlist
-    dTable = []
-    for i in notVisitedList:
-        temp = DField(i,-1,"Z") 
-        # '-1' means infinity and 'Z' means null-string
-        dTable.append(temp)
+    print("houseList[]: ")
+    houseList = getHouseList(stringList) # initialize a houselist into that variable
+    print(houseList)
+    
+    dTable = dTableInit(houseList) # initialize a dTable into that variable
     # print the entire dTable
     print("The D-Table:")
     for x in dTable:
         x.printValues()
 
-    # next step
-    visitedList = [] # to store houses that have been visited
-    visitedConnectionsList = []
+    visitedList = [] # list of houses that have been visited
 
-    # let distance of start vertex from start vertex = 0
-    # whichever is the 1st house in dTable, is the start vertex
+    # set the distance from start vertex to start vertex as 0
     dTable[0].shortest = 0
-    # let distance of all other vertices from start = inf
-    # which is done above
-    while true:
-        print("New Iteration:")
-        print("The updated D-Table:")
+    # the shortest distance of all other vertices have already been
+    # set to inf == -1
+    
+
+    while True:
+        currentVertexIndex = findVertexShortestDistance(dTable,visitedList)
+        # ↑ also checks visitedList, if all visited then returns '-1'
+        currentVertex = dTable[currentVertexIndex].house
+        currentVertexShortest = dTable[currentVertexIndex].shortest
+
+        # terminate loop if visitedList is full
+        if currentVertexIndex == -1: break
+
+
+        neighboursPosition = [] # line position in the stringList
+        neighbours = [] # list of the actual neighbours (strings)
+        neighboursDistanceValue = [] # corresponding list of the neighbour's distance values
+
+        # find the unvisited neighbours of this vertex and store in list
+        for index, i in enumerate(stringList):
+            line = i.split(",")
+            if line[1] == currentVertex:
+                if line[2] in visitedList: continue
+                neighbours.append(line[2])
+                neighboursPosition.append(index)
+                neighboursDistanceValue.append(int(line[0]))
+            # now for the reverse order, find neighbours
+            if line[2] == currentVertex:
+                if line[1] in visitedList: continue
+                neighbours.append(line[1])
+                neighboursPosition.append(index)
+                neighboursDistanceValue.append(int(line[0]))
+        print ("The neighbours of " + currentVertex + " are")
+        print(neighbours)
+
+        # if the calculatedDistance is shorter than the neighbour's distance, update values
+        # in dTable for those neighbours
+        for index, i in enumerate(neighbours):
+            calculatedDistance = currentVertexShortest + neighboursDistanceValue[index]
+            # -1 means infinity, so just put that as the new distance value and skip loop
+            if dTable[findInTable(dTable,i)].shortest == -1:
+                dTable[findInTable(dTable,i)].shortest = calculatedDistance
+                # also update the prevHouse value for the neighbours entries in the dTable
+                dTable[findInTable(dTable,i)].prevHouse = currentVertex
+                continue
+            if calculatedDistance < dTable[findInTable(dTable,i)].shortest:
+                dTable[findInTable(dTable,i)].shortest = calculatedDistance
+                # also update the prevHouse value for the neighbours entries in the dTable
+                dTable[findInTable(dTable,i)].prevHouse = currentVertex
+                continue
+
+        # print the updated values
+        print("Latest dTable is:")
         for x in dTable:
             x.printValues()
+            
+        # now add the current vertex to the list of visited vertices
+        visitedList.append(currentVertex)
+        print("VisitedList (updated)")
+        print(visitedList)
+    
 
-        min = -1
-        foundIndex = -1
-        for idx, i in enumerate(dTable):
-            if min == -1:
-                min = i.shortest
-                foundIndex = idx
-                continue
-            if i.shortest == -1: # inf cannot be min, so goto next
-                continue
-            if i.shortest < min:
-                min = i.shortest
-                foundIndex = idx
-        print("lowest is: " + dTable[foundIndex].house)
-        if dTable[foundIndex].house in visitedList:
-            continue # goto next if this house already visited
 
-        # for this unvisited vertex, examine its neighbours
-        currentHouse = dTable[foundIndex].house
-        currentConnection = ""
-        for x in stringList:
-           i = x.split(",")
-           if i[1] == currentHouse:
-               neighbour = i[2]
-               currentConnection = i[1]+i[2]
-               # now check if this connection is already visited
-               if currentConnection not in visitedConnectionsList:
+if __name__ == "__main__":
+     djikstraModule()
 
